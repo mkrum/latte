add0ea02d28b3bbfccb0bb2abf62f71a3b2f3380
Matthew Lanus <matt.lanus@gmail.com>
2016-12-06 01:17:51 +0000

    Have the general output working - will fix errors tomorrow

---
+++ include/layers/output.h
@@ -0,0 +1,26 @@
+#ifndef OUTPUT_H 
+#define OUTPUT_H
+
+#include "../layer.h"
+#include "../matrix.h"
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cstdio>
+
+using std::string;
+using std::vector;
+
+class Output : public Layer {
+  public:
+    Output(string, vector<string>, vector<string>, vector<string>);
+    Matrix &forward(Matrix&);
+    Matrix &backward(Matrix&);
+    string name;
+    vector<string> prev;
+    vector<string> next;
+  private:  
+
+};
+
+#endif
