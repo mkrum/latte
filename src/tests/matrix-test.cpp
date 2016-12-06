3617ae1f9cf8e06fb9de627c07f9a15111d67df9
Tristan Mitchell <tmitche9@nd.edu>
2016-12-05 20:10:29 -0500

    Added matrix test file

--- src/tests/matrix-test.cpp
+++ src/tests/matrix-test.cpp
@@ -1,12 +1,28 @@
 #include "matrix.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  Matrix test({ 5, 5 });
-  Matrix org = test[0][0]; 
-  Matrix n_org = test[0][0]; 
+int main() {
+	double data[4] = { 3, 1, 4, 0 };
+	double data2[4] = { 2, 6, -2, 1.5 };
+	vector<size_t> shape;
+	shape.push_back(2);
+	shape.push_back(2);
 
+	Matrix test(data, shape);
+	Matrix test2(data2, shape);
+
+	test = test + test2;
+	cout << test.data[0] << " " << test.data[1] << " " << test.data[2] << " " << test.data[3] << endl;
+
+	test = test * 4.1;
+	cout << test.data[0] << " " << test.data[1] << " " << test.data[2] << " " << test.data[3] << endl;
+
+	test = test2 + test2;
+	cout << test.data[0] << " " << test.data[1] << " " << test.data[2] << " " << test.data[3] << endl;
+
+	test = test2 + 1.3;
+	cout << test.data[0] << " " << test.data[1] << " " << test.data[2] << " " << test.data[3] << endl;
 }
-
